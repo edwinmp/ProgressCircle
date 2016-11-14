71adf693c94de1600875dc76f798145fead18a96
Edwin P. Magezi <emagezi@gmail.com>
2016-11-14 14:46:59 +0300

    Build default circular progress bar

--- src/com/mendix/widget/ProgressCircle/components/Progress.ts
+++ src/com/mendix/widget/ProgressCircle/components/Progress.ts
@@ -1,7 +1,16 @@
+import "../ui/circle.less";
+import * as classNames from "classnames";
 import { DOM } from "react";
 
 interface ProgressProps {
     percentage: number;
 }
 
-export const Progress = (props: ProgressProps) => DOM.div(null, props.percentage + "%");
+export const Progress = (props: ProgressProps) =>
+    DOM.div({ className: classNames("c100", "p" + props.percentage, "center") },
+        DOM.span(null, props.percentage + "%"),
+        DOM.div({ className: "slice" },
+            DOM.div({ className: "bar" }),
+            DOM.div({ className: "fill" })
+        )
+    );
